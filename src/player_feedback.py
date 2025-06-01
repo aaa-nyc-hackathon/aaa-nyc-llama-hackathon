171df3642f80ec757bcee6b72e9cb5e609c9efe4
Lucas Roberts <rlucas@Lucass-MacBook.local>
2025-06-01 00:24:24 -0400

    add some player feedback from llama4

---
+++ src/player_feedback.py
@@ -0,0 +1,95 @@
+import base64
+import os
+import json
+import jsonlines
+
+from llama_api_client import LlamaAPIClient
+
+def image_to_base64(image_path):
+    with open(image_path, "rb") as img:
+        return base64.b64encode(img.read()).decode('utf-8')
+
+# check for access
+if not os.environ.get("LLAMA_API_KEY"):
+    raise ValueError(f"you need to obtain a LLAMA api key first")
+
+
+HERD = {"mav": "Llama-4-Maverick-17B-128E-Instruct-FP8",
+        "scout": "Llama-4-Scout-17B-16E-Instruct-FP8"}
+
+file = "frame6-boxed.jpg"
+
+## text i/o example
+client = LlamaAPIClient(
+    api_key=os.environ.get("LLAMA_API_KEY"), # This is the default and can be omitted
+)
+
+contents = [{"type": "text",
+             "text": "For the player in the box in the image, identify any good or bad things they are doing in the game.",
+            }]
+
+base_64_image = image_to_base64("images/" + file)
+image_data = {
+    "type": "image_url",
+    "image_url": {
+        "url": f"data:image/jpeg;base64,{base_64_image}"
+    },
+}
+contents.append(image_data)
+
+completion = client.chat.completions.create(
+    model=HERD["mav"],
+    messages=[
+        {
+            "role": "user",
+            "content": contents
+        }
+    ],
+    response_format = {
+     "type": "json_schema",
+     "json_schema": {
+     "schema": {
+      "title": "Llama Analysis Output",
+      "type": "object",
+      "properties": {
+        "key_observations": {
+          "type": "array",
+          "items": {
+            "type": "string",
+            "description": "A list of key observations from the analysis"
+          },
+          "description": "Key observations from the analysis",
+          "maxItems": 2
+        },
+        "potential_issues": {
+          "type": "array",
+          "items": {
+            "type": "string",
+            "description": "A list of potential issues identified during the analysis"
+          },
+          "description": "Potential issues identified during the analysis",
+          "maxItems": 2
+        },
+        "positives": {
+          "type": "array",
+          "items": {
+            "type": "string",
+            "description": "A list of positive aspects identified during the analysis"
+          },
+          "description": "Positive aspects identified during the analysis",
+          "maxItems": 2
+        },
+        "final_conclusion": {
+          "type": "string",
+          "description": "The final conclusion based on the analysis"
+        }
+      },
+      "required": ["key_observations", "potential_issues", "positives", "final_conclusion"]
+    }
+  }
+}
+)
+
+feedback = json.loads(completion.completion_message.content.text)
+with jsonlines.open("feedback_output.jsonl", mode='a') as writer:
+    writer.write(feedback)
