04a14cc6d4c7d99e0045f100e596bf8a9c39cab3
gg-blake <blake092202@gmail.com>
2025-06-17 20:32:02 -0400

    added cache folders for images and videos

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-TMP_DIRNAME = os.path.join(os.path.dirname(__file__), "experimental/tmp")
+TMP_DIRNAME_IMAGES = os.path.join(os.getcwd(), "images/cache/segmentation")
+TMP_DIRNAME_VIDEOS = os.path.join(os.getcwd(), "videos/cache/segmentation")
 MAX_FRAMES = 500
 HF_MODEL = "facebook/sam2-hiera-large"
 # HF_MODEL = "facebook/sam2-hiera-tiny"
